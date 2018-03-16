736522005b6908d26ce2f1fe22e88ae2893f17e0
t-sin <shinichi.tanaka45@gmail.com>
2018-03-16 23:56:42 +0900

    Embeds list reader into main reader function

--- lisc.py
+++ lisc.py
@@ -1,13 +1,10 @@
 #!/bin/python3
 
-def l_read_list(s):
-    return [l if c == ')' else l.append(l_read(s[i:])[0]) if c != ' ' else l for l in [[]] for (i, c) in enumerate(s)][-1]
-
 def l_read_symbol(s):
     return [(str, len(str)) for str in [l.append(''.join(n)) if c == ' ' or c == ')' else n.append(c) or l for n in [[]] for l in [[]] for c in s][0]][0][0]
 
 def l_read(s):
-    return [l_read_list(s[1:]) if s[0] == '(' else l_read_symbol(s) for s in [list(''.join(s).strip())]][0]
+    return [read_list(s[1:]) if s[0] == '(' else l_read_symbol(s) for s in [list(''.join(s).strip())] for read_list in [lambda s: [l if c == ')' else l.append(l_read(s[i:])[0]) if c != ' ' else l for l in [[]] for (i, c) in enumerate(s)][-1]]][0]
 #
 if __name__ == '__main__':
     print(l_read(list(input('> '))))

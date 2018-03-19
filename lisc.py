b4a2957a1b835bb17fc732027eeae7d58fe92505
t-sin <shinichi.tanaka45@gmail.com>
2018-03-20 00:47:35 +0900

    Debugging...

--- lisc.py
+++ lisc.py
@@ -1,12 +1,14 @@
 #!/bin/python3
 
 def read_list(s):
-    return [[(l,i) if c == ')' else print(l) or (l,i) if c == ' ' else [[clis.pop(0) for j in range(n)] and l.append(ss) or (l,i+n) for ss,n in [l_read(s[i:])]][0] for l in [[]] for i,c in clis] for clis in [list(enumerate(s))]][0][-1]
+    print('read-list: ',s)
+    return [[clis.clear() or (l,i) if c==')' else (l,i) if c==' ' else [[clis.pop(0) for j in range(n)] and l.append(ss) or (l,i+n) for ss,n in [l_read(s[i:])]][0] for l in [[]] for i,c in clis] for clis in [list(enumerate(s))]][0][0]
 
 def read_symbol(s):
-    return [(l.append((''.join(n), len(n)))) or l if c == ' ' or c == ')' else n.append(c) or l for n in [[]] for l in [[]] for c in s][0][0]
+    return [(l.append((''.join(n), len(n)-1))) or l if c == ' ' or c == ')' else n.append(c) or l for n in [[]] for l in [[]] for c in s][0][0]
 
 def l_read(s):
+    print('l_read:', s)
     return [read_list(s[1:]) if s[0] == '(' else read_symbol(s) for s in [list(''.join(s).strip())]][0]
 
 env = (None, {})

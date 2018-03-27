6b132ea330fa367fdd1b908ee88e3f7cdcce04e5
t-sin <shinichi.tanaka45@gmail.com>
2018-03-27 22:41:06 +0900

    `nil` is a empty list truly

--- lisc_parts.py
+++ lisc_parts.py
@@ -22,21 +22,21 @@ def l_read(stream):
 
 # constants and functions
 env = (None, {})
-env[1]['nil'] = 'nil'
+env[1]['nil'] = []
 env[1]['t'] = 't'
-env[1]['atom'] = ('lambda', None, lambda o: 't' if type(o) is not list else 'nil')
+env[1]['atom'] = ('lambda', None, lambda o: 't' if type(o) is not list else [])
 env[1]['cons'] = ('lambda', None, lambda a, b: [a, b])
 env[1]['car'] = ('lambda', None, lambda l: l[0])
-env[1]['cdr'] = ('lambda', None, lambda l: l[1:] if len(l) > 1 else 'nil')
-env[1]['eq'] = ('lambda', None, lambda a, b: 't' if ('nil' in [a,b] and [] in [a,b]) or a == b else 'nil')
+env[1]['cdr'] = ('lambda', None, lambda l: l[1:] if len(l) > 1 else [])
+env[1]['eq'] = ('lambda', None, lambda a, b: 't' if ('nil' in [a,b] and [] in [a,b]) or a == b else [])
 
 # evaluator
 def l_eval(l, env=env):
-    return ('nil' if len(l) == 0 else (l_eval(l[2], env) if len(l) == 4 and l_eval(l[1], env) == 't' else l_eval(l[3], env)) if l[0] == 'if' else (l[1] if len(l) == 2 else '__quote_invalid_argument__') if l[0] == 'quote' else (('lambda', l[1], lambda new_env: l_eval(l[2], (env, new_env))) if len(l) == 3 else '__invalid_lambda_expression__') if l[0] == 'lambda' else (env[1].update({l[1]: l_eval(l[2], env)}) or (env[1][l[1]] if len(l) == 3 else '__define_invalid_argument__')) if l[0] == 'define' else [[fn[2](*eval_args) if fn[1] is None else fn[2](dict(zip(fn[1], eval_args))) if len(fn[1])+1 == len(l) else '__wrong_number_of_args__' for eval_args in [[l_eval(a, env) for a in l[1:]]]][0] if type(fn) is tuple and fn[0] == 'lambda' else '__undefined_operator__' for fn in [l_eval(l[0], env)]][0]) if type(l) is list else [search_val(env, l) for _ in [None] for search_val in [lambda e, s: [v if v else '__unbound_variable__' if e[0] is None else search_val(e[0], s) for v in [e[1].get(s, None)]][0]]][0] if type(l) is str else '__invalid_object__'
+    return ('nil' if len(l) == 0 else (l_eval(l[2], env) if len(l) == 4 and l_eval(l[1], env) == 't' else l_eval(l[3], env)) if l[0] == 'if' else (l[1] if len(l) == 2 else '__quote_invalid_argument__') if l[0] == 'quote' else (('lambda', l[1], lambda new_env: l_eval(l[2], (env, new_env))) if len(l) == 3 else '__invalid_lambda_expression__') if l[0] == 'lambda' else (env[1].update({l[1]: l_eval(l[2], env)}) or (env[1][l[1]] if len(l) == 3 else '__define_invalid_argument__')) if l[0] == 'define' else [[fn[2](*eval_args) if fn[1] is None else fn[2](dict(zip(fn[1], eval_args))) if len(fn[1])+1 == len(l) else '__wrong_number_of_args__' for eval_args in [[l_eval(a, env) for a in l[1:]]]][0] if type(fn) is tuple and fn[0] == 'lambda' else '__undefined_operator__' for fn in [l_eval(l[0], env)]][0]) if type(l) is list else [search_val(env, l) for _ in [None] for search_val in [lambda e, s: [v if v is not None else '__unbound_variable__' if e[0] is None else search_val(e[0], s) for v in [e[1].get(s, None)]][0]]][0] if type(l) is str else '__invalid_object__'
 
 # printer
 def l_print(l):
-    return '(' + ' '.join([l_print(e) for e in l]) + ')' if type(l) is list or type(l) is tuple else str(l)
+    return 'nil' if l == [] else '(' + ' '.join([l_print(e) for e in l]) + ')' if type(l) is list or type(l) is tuple else str(l)
 
 if __name__ == '__main__':
     [b.append(None) or print(l_print(l_eval(l_read(_make_stream(input('> ')))))) for b in [[None]] for a in b]
--- lisc_ref.py
+++ lisc_ref.py
@@ -39,18 +39,18 @@ def l_read(s):
         return l_read_symbol(s2)
 
 env = (None, {})
-env[1]['nil'] = 'nil'
+env[1]['nil'] = []
 env[1]['t'] = 't'
-env[1]['atom'] = ('lambda', None, lambda o: 't' if type(o) is not list else 'nil')
+env[1]['atom'] = ('lambda', None, lambda o: 't' if type(o) is not list else [])
 env[1]['cons'] = ('lambda', None, lambda a, b: [a, b])
 env[1]['car'] = ('lambda', None, lambda l: l[0])
-env[1]['cdr'] = ('lambda', None, lambda l: l[1:] if len(l) > 1 else 'nil')
-env[1]['eq'] = ('lambda', None, lambda a, b: 't' if ('nil' in [a,b] and [] in [a,b]) or a == b else 'nil')
+env[1]['cdr'] = ('lambda', None, lambda l: l[1:] if len(l) > 1 else [])
+env[1]['eq'] = ('lambda', None, lambda a, b: 't' if ('nil' in [a,b] and [] in [a,b]) or a == b else [])
 
 def l_eval(l, env=env):
     if type(l) is list:
         if len(l) == 0:
-            return 'nil'
+            return []
         elif l[0] == 'if':
             return l_eval(l[2], env) if len(l) == 4 and l_eval(l[1], env) == 't'else l_eval(l[3], env)
         elif l[0] == 'quote':
@@ -74,13 +74,13 @@ def l_eval(l, env=env):
                 return '__undefined_operator__'
     elif type(l) is str:
         def _search_val(e, s):
-            return ['__unbound_variable__' if e is None else v if v else _search_val(e[0], s) for v in [e[1].get(s, None)]][0]
+            return ['__unbound_variable__' if e is None else v if v is not None else _search_val(e[0], s) for v in [e[1].get(s, None)]][0]
         return _search_val(env, l)
     else:
         return '__invalid_object__'
 
 def l_print(l):
-    return '(' + ' '.join([l_print(e) for e in l]) + ')' if type(l) is list or type(l) is tuple else str(l)
+    return 'nil' if l == [] else '(' + ' '.join([l_print(e) for e in l]) + ')' if type(l) is list or type(l) is tuple else str(l)
 
 if __name__ == '__main__':
     [b.append(None) or print(l_print(l_eval(l_read(input('> '))[0]))) for b in [[None]] for a in b]
